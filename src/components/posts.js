b503a8e80f1583e0bf47ce7307686f2dfefe95ab
yohannes11 <johannes.neg@gmail.com>
2021-04-17 16:12:11 +0300

    day 2 part 5
    day 2 part 5

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const Posts = () =>{
     return(
         <div>
-            Posts
+           <Link to="/posts/1">post 1</Link>
+           <Link to="/posts/2">post 2</Link>
+           <Link to="/posts/3">post 3</Link>
         </div>
     )
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter,Route,Link} from 'react-router-dom'
 import Home from './components/home'
 import Posts from './components/posts'
 import Profile from './components/profile'
-
+import PostItem from './components/post_item'
 
 const App = () =>{
   return (
@@ -17,7 +17,8 @@ const App = () =>{
      <Link to={{pathname:'/profile'}}>Profile</Link>
      </header>
  <Route path="/" exact component={Home}/>
- <Route path="/posts" component={Posts}/>
+ <Route path="/posts" exact component={Posts}/>
+ <Route path="/posts/:id/:username" component={PostItem}/>
  <Route path="/profile" component={Profile}/>
  </div>
  </BrowserRouter>
